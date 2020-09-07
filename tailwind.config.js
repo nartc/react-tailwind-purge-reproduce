0147f43c585ca99c0d5cff89b70ff73b38a5b09b
Chau Tran <ctch5@mail.umsl.edu>
2020-09-07 07:06:15 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  prefix: '',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: true,
+    content: ['**/*.jsx', '**/*.js'],
+  },
+  theme: {},
+};
